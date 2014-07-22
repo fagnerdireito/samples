b9b540df48b2d73305ebe36c43c21980696a7685
alebarros <alebarros.vha@gmail.com>
2014-07-22 09:43:19 -0300

    Update protocolosequencial.sql

--- protocolosequencial.sql
+++ protocolosequencial.sql
@@ -1,8 +1,8 @@
-sql = " BEGIN;" _
-    & " LOCK TABLE protocolo WRITE, protocolo as temp1 READ; " _
-    & " SET @ultimoprotocolo = (SELECT MAX(protocolo) protocolo from protocolo as temp1 where exercicio= year(CURDATE()) limit 1);" _
-    & " SET @protocolonovo = IF (@ultimoprotocolo IS NULL, CONCAT_WS('',YEAR(CURDATE()),'000001'),@ultimoprotocolo + 1); " _
-    & " INSERT INTO protocolo set protocolo=@protocolonovo, exercicio=year(CURDATE()), data_create=CURDATE();" _
-    & " select @protocolonovo as protocolo;" _
-    & " UNLOCK TABLE;" _
-    & " COMMIT;"
+BEGIN;
+LOCK TABLE protocolo WRITE, protocolo as temp1 READ; 
+SET @ultimoprotocolo = (SELECT MAX(protocolo) protocolo from protocolo as temp1 where exercicio= year(CURDATE()) limit 1);
+SET @protocolonovo = IF (@ultimoprotocolo IS NULL, CONCAT_WS('',YEAR(CURDATE()),'000001'),@ultimoprotocolo + 1); 
+INSERT INTO protocolo set protocolo=@protocolonovo, exercicio=year(CURDATE()), data_create=CURDATE();
+select @protocolonovo as protocolo;
+UNLOCK TABLE;
+COMMIT;
